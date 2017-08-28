23f5f5fcb3080c7ceffb608eb6c8756c0ce78d39
ahwelgemoed <arno.welgemoed@outlook.com>
2017-08-28 16:09:18 +0200

    Video Working

--- projects/Unsea/js/main.js
+++ projects/Unsea/js/main.js
@@ -45,78 +45,123 @@ $(".dogicon").click(function(){
 
     }, 1000);
 }); 
-// myVideo[0].load()   //if there is only one
-
-// for(var i=0;i<myVideo.length;i++){
-//     myVideo[i].load();    //if there is more
-// }
-
-// var videos = [
+// window.onload =  function build(){
+//     var videos = [
 //     "video/sea.mp4",
 //     "video/1.mp4",
 //     "video/2.mp4",
 //     "video/3.mp4"
 // ];
+//     var random = Math.floor(Math.random()* videos.length);
+//     var videos1 = videos[random]; // Changes random selected value to corresponding name in index//
+//     var video = document.getElementById('stuff');
+//     var source = document.createElement('source');
+//     var audio = document.getElementById('Jonk')
 
-// function playArray(index,ele,array,listener){
-//     ele.removeEventListener(listener||0);
-//     ele.src = array[index];
-//     ele.load();
-//     ele.play();
-//     index++;
-//     if(index>=array.length){
-//         index=0;
-//     }
-//     listener = ele.addEventListener('ended',function(){
-//         playArray(index,ele,array,listener);
-//     },false);
-// }
-// playArray(0,document.getElementById("myVid"),videos);
-window.onload =  function build(){
-    var videos = [
-    "video/sea.mp4",
+//     source.removeAttribute('src');
+//     source.setAttribute('src', videos1);
+//     video.appendChild(source);
+//     video.play();
+
+// video.addEventListener("ended", function(){
+//     build();
+//         // build(0,document.getElementById("video"),videos);
+//         source.setAttribute('src', videos1);
+//         video.play();
+
+    
+
+//   });
+// };
+function shuffle(array)
+{
+  var m = array.length, t, i;
+  while (m > 0) 
+  {
+	i = Math.floor(Math.random() * m--);
+	t = array[m];
+	array[m] = array[i];
+	array[i] = t;
+  }
+  return array;
+}
+
+var videos = [
     "video/1.mp4",
     "video/2.mp4",
-    "video/3.mp4"
+    "video/3.mp4",
+    "video/Fire_1.mp4",
+    "video/Fire_2.mp4",
+    "video/Fire_3.mp4",
+    "video/Face_1.mp4",
+    "video/Face_2.mp4",
 ];
+
+
+var video = document.getElementById('stuff');
+var audio = document.getElementById('Jonk');
+var duration
+var current
+function build(){
+
+shuffle(videos); // shuffles the array
+
     var random = Math.floor(Math.random()* videos.length);
-    var videos1 = videos[random]; // Changes random selected value to corresponding name in index//
 
+        var videos1 = videos[random]; // Changes random selected value to corresponding name in index//
 
-    var video = document.getElementById('stuff');
-    var source = document.createElement('source');
-    var audio = document.getElementById('Jonk')
-
-
-    source.setAttribute('src', videos1);
-    video.appendChild(source);
-    video.play();
-
-  video.addEventListener("ended", function(){
-    
-
-  });
-
-
-
-
-    //     // Inputs Colour and Border//
-    //    for(var i = 0; i < video.length; i++)
-    //         {
-    //             var random = Math.floor(Math.random()* videos.length);
-    //             var videos1 = videos[random]; // Changes random selected value to corresponding name in index//
-
-            
-            
-    //             source.setAttribute('src', videos1);
-    //             video.appendChild(source);
-    //             video.play();
-           
-            // }
-
-
-
-            // });
-    
-
+        video.setAttribute('src', videos1);
+//      console.log(videos1);
+        video.play();
 }
+video.addEventListener("ended", function(){
+    
+    build();
+});
+window.onload = build();
+ 
+audio.addEventListener("ended", function(){
+    $("#redbox1").animate({opacity: '0'}
+);
+$("#redbox2").animate({opacity: '0'}
+);
+$("#redbox3").animate({opacity: '0'}
+);
+$("#redbox4").animate({opacity: '0'}
+);
+$(".dogicon").animate({opacity: '0'}
+);
+$("#bgvid").animate({opacity: '0'});
+$( "audio" ).remove();
+
+setTimeout(function() {
+    $( "#redbox1" ).remove();
+    $( "#redbox2" ).remove();
+    $( "#redbox3" ).remove();
+    $( "#redbox4" ).remove();
+    $( ".dogicon" ).remove();
+    $( "#bgvid" ).remove();
+    $(".off1").addClass("animated fadeInDown mainstage ")
+    $(".off2").addClass("animated fadeInDown jamesstage")
+    $(".off3").addClass("animated fadeInDown food")
+
+
+}, 1000);
+
+});
+
+
+
+// video.addEventListener("progress", function() {
+//     // When buffer is 1 whole video is buffered
+//     if (Math.round(video.buffered.end(0)) / Math.round(video.seekable.end(0)) === 1) {
+//         build();
+//    }
+//   }, false);
+
+// var duration = video.duration
+// var current = video.currentTime
+
+
+
+
