6d4b46ca737c11253e160abb5ed221940a23a856
ahwelgemoed <arno.welgemoed@outlook.com>
2017-09-01 10:33:20 +0200

    Promo
    Added Promo Code Based On Clicks.
    added modal to Linup, Still needs for Booking and Rules. Lets make it spicy

--- projects/Unsea/js/main.js
+++ projects/Unsea/js/main.js
@@ -19,9 +19,9 @@ var aud = document.getElementById("Jonk");
 var clicks = 0;
 
 $('.dogicon').click(function () {
-// If time is past Poem Part it makes so that the Button only needs one click too skip Music Video //
+    // If time is past Poem Part it makes so that the Button only needs one click too skip Music Video //
     if (aud.currentTime > 195) {
-        
+
         ++clicks;
     };
     if (clicks == 0) {
@@ -222,37 +222,85 @@ $("#Jonk").bind("timeupdate", function () {
     }
 });
 
-$("#lineup").hover(function(){
+$("#lineup").hover(function () {
     $(this).css("width", "250px");
-    }, function(){
+}, function () {
     $(this).css("width", "150px");
 });
-$("#rules").hover(function(){
+$("#rules").hover(function () {
     $(this).css("width", "250px");
-    }, function(){
+}, function () {
     $(this).css("width", "150px");
 });
-$("#book").hover(function(){
+$("#book").hover(function () {
     $(this).css("width", "250px");
-    }, function(){
+}, function () {
     $(this).css("width", "150px");
 });
- 
+
 // Cheacks Aspectratio of Browser and Adds and removes Divs acordingly //
 
-$(window).resize(function() {
-    var aspectratio = $(window).width()/$(window).height()
-    if (aspectratio <= 1 || aspectratio >= 1.5){
+$(window).resize(function () {
+    var aspectratio = $(window).width() / $(window).height()
+    if (aspectratio <= 1 || aspectratio >= 1.5) {
         $(".good").hide();
         $(".next").hide();
         $("#viewertext").show();
         $(".browsergif").show();
-    }else{
+    } else {
         $(".good").show()
         $(".next").show()
         $(".browsergif").hide();
         $("#viewertext").hide();
     }
 }).resize();
+var click = 0
+// Promo For clicking on all Links //
 
-$("#lineupModal").animatedModal();
+$('#lineupModalClick, #rulesModal, #bookModal').click(function() {
+
+    setTimeout(function(){
+    if (click === 3){
+            alert("You Seem too Like Us. Use Promo Code for 10% off on Kreef Hotel 56040349f");
+            $(this).remove()
+        }
+    }, 5000)
+    if (this.id == 'lineupModalClick') {
+
+        ++click;
+    } else if (this.id == 'rulesModal') {
+
+        ++click;
+    } else if (this.id == 'bookModal') {
+        ++click;
+    }
+
+ 
+});
+// Modals
+
+// Get the modal
+var modal = document.getElementById('lineupModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("lineupModalClick");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function () {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
