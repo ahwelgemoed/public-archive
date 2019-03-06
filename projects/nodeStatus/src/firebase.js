9ad9ea3030c2c3ed185babe1f32f5c992e18e50a
AHWelgemoed <arno.welgemoed@outlook.com>
2019-03-06 09:10:21 +0200

    added ENV

--- projects/nodeStatus/src/firebase.js
+++ projects/nodeStatus/src/firebase.js
@@ -1,12 +1,26 @@
 import firebase from 'firebase';
-const config = {
-  apiKey: 'AIzaSyAN0griPsgIMDhWdHbB17L14xQb08amcG4',
-  authDomain: 'mtonode.firebaseapp.com',
-  databaseURL: 'https://mtonode.firebaseio.com',
-  projectId: 'mtonode',
-  storageBucket: 'mtonode.appspot.com',
-  messagingSenderId: '535542954851'
+require('dotenv').config();
+
+const prodConfig = {
+  apiKey: process.env.REACT_APP_PROD_API_KEY,
+  authDomain: process.env.REACT_APP_PROD_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_PROD_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROD_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_PROD_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_PROD_MESSAGING_SENDER_ID
 };
+
+const devConfig = {
+  apiKey: process.env.REACT_APP_DEV_API_KEY,
+  authDomain: process.env.REACT_APP_DEV_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DEV_DATABASE_URL,
+  projectId: process.env.REACT_APP_DEV_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_DEV_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_DEV_MESSAGING_SENDER_ID
+};
+
+const config = process.env.NODE_ENV === 'production' ? prodConfig : devConfig;
+
 var Firebase = firebase.initializeApp(config);
 // export default fire;
 
