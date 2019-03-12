f0f0fc5acd46778f170cad3a2136fba239b2980f
AHWelgemoed <arno.welgemoed@outlook.com>
2019-03-12 17:01:54 +0200

    Reload Page every 12 H

--- projects/nodeStatus/src/App.js
+++ projects/nodeStatus/src/App.js
@@ -6,6 +6,26 @@ import { Offline, Online } from 'react-detect-offline';
 import './App.css';
 
 class App extends Component {
+  state = { count: 1 };
+  tick() {
+    this.setState({ count: this.state.count + 1 });
+    if (this.state.count >= 43200) {
+      window.location.reload(true);
+    }
+  }
+  startTimer() {
+    clearInterval(this.timer);
+    this.timer = setInterval(this.tick.bind(this), 1000);
+  }
+  componentDidMount() {
+    this.startTimer();
+    fetch('https://socket.mto.group:1337/', {
+      headers: new Headers({ 'content-type': 'application/json' }),
+      mode: 'no-cors'
+    }).then(function(response) {
+      console.log(response);
+    });
+  }
   render() {
     return (
       <div className="">
