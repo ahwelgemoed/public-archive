00fd4e4b7d3e613e7fc6f38d7c954a7d461e633f
Arno-Macbook <arno.welgemoed@outlook.com>
2019-03-24 20:50:28 +0200

    TestTing Client Socket :100:

--- projects/canIRideServer/index.js
+++ projects/canIRideServer/index.js
@@ -9,18 +9,16 @@ const server = http.createServer(app);
 const io = socketIO(server);
 
 io.on('connection', socket => {
+  let wetTime;
   console.log('New client connected'),
     getApiAndEmit(socket),
-    setInterval(() => morningCheck(socket), 10000);
+    socket.on('wetTime', inComing => {
+      // console.log(inComing);
+      wetTime = inComing;
+    });
+  setInterval(() => morningCheck(socket, wetTime), 10000);
 
-  socket.on('setAlarm', inComing => {
-    console.log(inComing);
-    const val = inComing + 1;
-    console.log(val);
-    io.sockets.emit('Listen', val);
-  });
-
-  socket.on('disconnect', () => console.log('Client disconnected'));
+  socket.on('disconnect', () => console.log('Client disconcted'));
 });
 
 const getApiAndEmit = async socket => {
@@ -34,10 +32,12 @@ const getApiAndEmit = async socket => {
       return res;
     });
 };
-const morningCheck = async socket => {
+const morningCheck = async (socket, wetTime) => {
+  // console.log(wetTime);
+
   const TimeNow = moment(Date.now()).format('HH:mm');
   const SetTime = '15:58';
-  // Sees if Time is past 6 in morning
+  // Sees if Time is past 6 in morning The Goes To API TO GET TODAYS RAIN
   if (SetTime <= TimeNow) {
     await axios
       .get(
@@ -46,10 +46,13 @@ const morningCheck = async socket => {
       .then(function(response) {
         const res = response.data;
         const rainForTheDay = res.forecast.forecastday[0].day.totalprecip_mm;
+        // IF RAIN THEM PASS TRUE ELSE PASS FALSE
         if (rainForTheDay >= 0) {
-          itsRain(socket, true);
+          itsRain(socket, true, wetTime);
+          console.log('Yes');
         } else {
-          itsRain(socket, false);
+          itsRain(socket, false, wetTime);
+          console.log('Yes');
         }
       });
   } else {
@@ -57,16 +60,20 @@ const morningCheck = async socket => {
   }
 };
 
-const itsRain = (socket, val) => {
+const itsRain = (socket, val, wetTime) => {
   const TimeNow = moment(Date.now()).format('HH:mm');
-  const SetTime = '16:30';
-  if (val && TimeNow == SetTime) {
-    socket.emit('LETALARMOFF', true);
+  const SetTime = '20:47';
+  console.log(wetTime);
+
+  // IF RAIN AND NOW TIME = TO WHENALARM SHOUD GO OFF FOR RAIN
+  if (val && TimeNow == wetTime) {
     console.log('Sun Alarm');
-  }
-  if (!val && TimeNow == '16:31') {
     socket.emit('LETALARMOFF', true);
+  }
+  // IF NO RAIN AND NOW TIME = TO WHENALARM SHOUD GO OFF FOR NO RAIN
+  if (!val && TimeNow == '16:31') {
     console.log('Rain Alarm');
+    socket.emit('LETALARMOFF', true);
   }
 };
 server.listen(port, () => console.log(`Listening on port ${port}`));
