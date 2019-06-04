46c1664d8952968d8ca15ddcf17387f08aef4f2d
Arno <arno.welgemoed@outlook.com>
2019-06-04 17:48:06 +0200

    Updated Auth and Drawer

--- projects/dnjRN/screens/DrawerScreen.js
+++ projects/dnjRN/screens/DrawerScreen.js
@@ -42,87 +42,98 @@ class DrawerScreen extends Component {
   render() {
     const { profile } = this.props;
     return (
-      <Container style={styles.container}>
-        <Content>
-          <ListItem>
-            <Text style={styles.label}>
-              <OnlineUsers />
-            </Text>
-          </ListItem>
-          <ListItem onPress={this.changeTab.bind(this, 'Home')}>
-            <Icon style={styles.icons} name="home" />
-            <Text style={styles.label}> Home</Text>
-          </ListItem>
-          <ListItem onPress={this.changeTab.bind(this, 'Post')}>
-            <Icon style={styles.icons} name="clipboard" />
-            <Text style={styles.label}> Post</Text>
-          </ListItem>
-          {/* <ListItem onPress={this.changeTab.bind(this, 'YourPoems')}>
-            <Icon style={styles.icons} name="person" />
-            <Text style={styles.label}> Your Poems</Text>
-          </ListItem> */}
+      <View style={styles.container}>
+        {/* <Content> */}
+        <ListItem>
+          <Text style={styles.label}>
+            <OnlineUsers />
+          </Text>
+        </ListItem>
+        <ListItem onPress={this.changeTab.bind(this, 'Home')}>
+          <Icon style={styles.icons} name="home" />
+          <Text style={styles.label}> Home</Text>
+        </ListItem>
+        <ListItem onPress={this.changeTab.bind(this, 'Post')}>
+          <Icon style={styles.icons} name="clipboard" />
+          <Text style={styles.label}> Post</Text>
+        </ListItem>
+        <ListItem onPress={this.changeTab.bind(this, 'Bookmark')}>
+          <Icon style={styles.icons} name="bookmarks" />
+          <Text style={styles.label}> Bookmarks</Text>
+        </ListItem>
 
-          <ListItem onPress={this.changeTab.bind(this, 'Bookmark')}>
-            <Icon style={styles.icons} name="bookmarks" />
-            <Text style={styles.label}> Bookmarks</Text>
-          </ListItem>
+        <ListItem
+          onPress={() => WebBrowser.openBrowserAsync('https://klyntji.com/')}
+        >
+          <Icon style={styles.icons} name="heart" />
+          <Text style={styles.label}> KLYNTJI</Text>
+        </ListItem>
 
-          <ListItem
-            onPress={() => WebBrowser.openBrowserAsync('https://klyntji.com/')}
-          >
-            <Icon style={styles.icons} name="heart" />
-            <Text style={styles.label}> KLYNTJI</Text>
-          </ListItem>
-
-          <ListItem
-            onPress={() =>
-              WebBrowser.openBrowserAsync(`https://www.instagram.com/disnetjy`)
-            }
-          >
-            <Icon style={styles.icons} name="logo-instagram" />
-            <Text style={styles.label}> Follow DNJ</Text>
-          </ListItem>
-          <ListItem onPress={this.changeTab.bind(this, 'Share')}>
-            <Icon style={styles.icons} name="share" />
-            <Text style={styles.label}> Share DNJ</Text>
-          </ListItem>
-          <ListItem onPress={this.changeTab.bind(this, 'Account')}>
-            <Icon style={styles.icons} name="key" />
-            <Text style={styles.label}> Account Page</Text>
-          </ListItem>
-          <ListItem onPress={this.signOut}>
+        <ListItem
+          onPress={() =>
+            WebBrowser.openBrowserAsync(`https://www.instagram.com/disnetjy`)
+          }
+        >
+          <Icon style={styles.icons} name="logo-instagram" />
+          <Text style={styles.label}> Follow DNJ</Text>
+        </ListItem>
+        <ListItem onPress={this.changeTab.bind(this, 'Share')}>
+          <Icon style={styles.icons} name="share" />
+          <Text style={styles.label}> Share DNJ</Text>
+        </ListItem>
+        <ListItem onPress={this.changeTab.bind(this, 'Account')}>
+          <Icon style={styles.icons} name="key" />
+          <Text style={styles.label}> Account Page</Text>
+        </ListItem>
+        {profile.auth ? (
+          <View>
+            <ListItem itemDivider>
+              <Text>ADMIN ONLY</Text>
+            </ListItem>
+            <ListItem onPress={this.changeTab.bind(this, 'YourPoems')}>
+              <Icon style={styles.icons} name="person" />
+              <Text style={styles.label}> Your Poems</Text>
+            </ListItem>
+            <ListItem onPress={this.activateDeleteAction}>
+              <Icon style={styles.icons} name="person" />
+              {this.props.activateDelete ? (
+                <Text style={styles.label}> Deactivate Admin Rights</Text>
+              ) : (
+                <Text style={styles.label}> Activate Admin Rights</Text>
+              )}
+            </ListItem>
+          </View>
+        ) : null}
+        {/* </Content> */}
+        <View style={styles.bottom}>
+          <ListItem style={{ borderBottomWidth: 0, borderTopWidth: 0 }}>
             <Icon style={styles.icons} name="log-out" />
             <Text style={styles.label}> Sign Out</Text>
           </ListItem>
-          {profile.auth ? (
-            <View>
-              <ListItem onPress={this.activateDeleteAction}>
-                <Icon style={styles.icons} name="person" />
-                {this.props.activateDelete ? (
-                  <Text style={styles.label}> Deactivate Admin Rights</Text>
-                ) : (
-                  <Text style={styles.label}> Activate Admin Rights</Text>
-                )}
-              </ListItem>
-            </View>
-          ) : null}
-        </Content>
-      </Container>
+        </View>
+      </View>
     );
   }
 }
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginTop: 40
+    paddingTop: 40,
+    backgroundColor: '#efefef'
   },
   icons: {
-    fontSize: 12
+    fontSize: 12,
+    paddingRight: 10
   },
   label: {
-    fontSize: 16,
-    fontFamily: 'raleway-regular',
+    fontSize: 14,
+    fontFamily: 'raleway-extralight',
     textAlign: 'left'
+  },
+  bottom: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    marginBottom: 36
   }
 });
 
