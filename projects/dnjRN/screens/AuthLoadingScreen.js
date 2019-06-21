68ea7df9089732a6bd6cec7fc784a2ef041809f7
Arno <arno.welgemoed@outlook.com>
2019-06-21 19:02:02 +0200

    Dark mode on Auth Loading

--- projects/dnjRN/screens/AuthLoadingScreen.js
+++ projects/dnjRN/screens/AuthLoadingScreen.js
@@ -12,15 +12,19 @@ import { compose } from 'redux';
 import { connect } from 'react-redux';
 import { withFirebase, isLoaded, isEmpty } from 'react-redux-firebase';
 import { AppLoading, Asset, Font, Icon } from 'expo';
+import { DangerZone } from 'expo';
+import wiggly from '../components/data.json';
+import { ScreenBackground, PoemName } from '../components/Styles';
 
 class AuthLoadingScreen extends React.Component {
-  state = { firstVisit: null };
+  state = { firstVisit: null, animation: null, speed: 1, modalVisible: false };
+
   constructor(props) {
     super(props);
   }
-  componentDidMount() {
-    // this._bootstrapAsync();
-  }
+  // componentDidMount() {
+  //   // this._bootstrapAsync();
+  // }
   // Fetch the token from storage then navigate to our appropriate place
   _bootstrapAsync = async () => {
     const userToken = await AsyncStorage.getItem('userToke');
@@ -29,26 +33,20 @@ class AuthLoadingScreen extends React.Component {
       firstVisit
     });
   };
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.modalVisible === true && prevState.modalVisible === false) {
+      this._playAnimation();
+    }
+  }
+
+  setModalVisible(visible) {
+    this.setState({ modalVisible: visible });
+  }
 
   render() {
     const { auth } = this.props;
     if (!isLoaded(auth)) {
-      return (
-        <View>
-          <StatusBar barStyle="default" />
-          <Image
-            source={require('../assets/images/Loading.gif')}
-            style={{
-              width: 100,
-              height: 100,
-              flex: 1,
-              alignItems: 'center',
-              marginBottom: 20,
-              paddingLeft: 30
-            }}
-          />
-        </View>
-      );
+      return <ScreenBackground style={styles.mainContent} />;
     }
     if (isEmpty(auth)) {
       if (this.state.firstVisit !== 'Yes') {
@@ -60,6 +58,35 @@ class AuthLoadingScreen extends React.Component {
     return this.props.navigation.navigate('App');
   }
 }
+_changeSpeed = speed => {
+  this.setState({ speed });
+};
+
+_playAnimation = () => {
+  if (!this.state.animation) {
+    this._loadAnimation();
+  } else {
+    this.animation.reset();
+    this.animation.play();
+  }
+};
+
+_stopAnimation = () => {
+  this.animation.reset();
+};
+
+_loadAnimation = () => {
+  this.setState({ animation: wiggly }, this._playAnimation);
+};
+const styles = StyleSheet.create({
+  mainContent: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    paddingLeft: 12,
+    paddingRight: 12
+  }
+});
 
 export default compose(
   withFirebase,
