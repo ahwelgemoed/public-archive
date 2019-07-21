ea1fa3fec8433e005bfa2a64460eaf63a3b9b3c7
Arno <arno.welgemoed@outlook.com>
2019-07-21 17:25:36 +0200

    added Bookmarks

--- projects/dnjRN/components/NewPoem.js
+++ projects/dnjRN/components/NewPoem.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
+import { compose } from 'redux';
+import { connect } from 'react-redux';
+import { firestoreConnect } from 'react-redux-firebase';
+import { successfullyAddedPoem } from '../actions/poemsActions';
 import { Icon, Button } from 'native-base';
 import { StyledText, PoemName, PoemBodyText } from './Styles';
 import { Col, Row, Grid } from 'react-native-easy-grid';
@@ -7,6 +11,7 @@ import {
   CNRichTextView,
   getDefaultStyles
 } from 'react-native-cn-richtext-editor';
+import NewBookmark from './NewBookmark';
 const defaultStyles = getDefaultStyles();
 let customStyles = {
   ...defaultStyles,
@@ -16,8 +21,32 @@ let customStyles = {
   ol: { fontSize: 14 },
   ul: { fontSize: 12 }
 };
-export default class NewPoem extends Component {
-  state = { open: false };
+class NewPoem extends Component {
+  state = {
+    open: false,
+    userEdit: false,
+    bookmarked: false,
+    reportDialog: false,
+    modalVisible: false
+  };
+  componentDidMount() {
+    const { id } = this.props.poem;
+    if (this.props.profile.bookmarks) {
+      const found = this.props.profile.bookmarks.find(function(element) {
+        return element === id;
+      });
+      if (found) {
+        this.setState({
+          bookmarked: true
+        });
+      }
+    }
+  }
+  toggleBookMark = () => {
+    this.setState({
+      bookmarked: !this.state.bookmarked
+    });
+  };
   render() {
     return (
       <StyledText
@@ -27,18 +56,13 @@ export default class NewPoem extends Component {
       >
         <Row>
           <Col>
-            {/* TO:DO MOVE CODE TO BOOKMARK COMPONENT */}
-            <Icon
-              style={{
-                position: 'absolute',
-                color: '#474554',
-                top: 8,
-                right: -25,
-                transform: [{ rotate: '90deg' }]
-              }}
-              type="FontAwesome"
-              name="bookmark"
+            <NewBookmark
+              poemId={this.props.poem.id}
+              bookmarkedCount={this.props.poem.bookmarkedCount}
+              bookmarked={this.state.bookmarked}
+              toggleBookMark={this.toggleBookMark}
             />
+
             <PoemName>{this.props.poem.name}</PoemName>
             {this.props.poem.richText ? (
               <View
@@ -78,3 +102,16 @@ export default class NewPoem extends Component {
     );
   }
 }
+const mapStateToProps = state => ({
+  auth: state.firebase.auth,
+  profile: state.firebase.profile,
+  admin: state.poems.activateDelete,
+  theme: state.theme.isThemeDark
+});
+export default compose(
+  firestoreConnect(),
+  connect(
+    mapStateToProps,
+    { successfullyAddedPoem }
+  )
+)(NewPoem);
