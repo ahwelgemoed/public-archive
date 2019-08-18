2d05b3291085ab4c546de54828e81a115153daeb
Arno <arno.welgemoed@outlook.com>
2019-08-18 14:43:33 +0200

    🌓

--- projects/dnjRN/components/NewFeature.js
+++ projects/dnjRN/components/NewFeature.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Text, View, Dimensions, ScrollView, StyleSheet } from 'react-native';
-import Modal from 'react-native-modal';
+// import Modal from 'react-native-modal';
 import { InstagramText, FeatName, PoemBodyText } from './Styles';
 import { compose } from 'redux';
 import { connect } from 'react-redux';
